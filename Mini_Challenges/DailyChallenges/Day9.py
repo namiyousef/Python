cfc65ff43c926e9c687ad4224d9cfc21e84604a3
Yousef <namiyousef@hotmail.com>
2020-10-24 11:47:46 +0100

    added code for Day9

--- Mini_Challenges/DailyChallenges/Day9.py
+++ Mini_Challenges/DailyChallenges/Day9.py
@@ -2,30 +2,34 @@
 Author: Yousef Nami
 Problem definition: https://leetcode.com/problems/string-to-integer-atoi/
 Date started: 23.10.2020
-Date complete: 
+Date complete: 24.10.2020
 Results: 
+    - faster than 55.6% of submissions
+    - less memory than 99.98% of submissions
 """
 
 
 
 def convert_to_int(s = '1234'):
-    digits = "0123456789- "
+    digits = "0123456789-+ "
     if not s:
         return 0
     num = ""
-    for letter in s:
-        if letter not in digits:
-            num = 0 if (not num or num=='-') else num
-            break
-        elif letter != " ":
-            num+=letter
-    print(num)    
-    num = 2**31 - 1 if int(num) > 2**31 -1 else int(num)
-    num = -2**31 if int(num) < -2**31 else int(num)
+    i = 0
+    while i<len(s) and s[i] in digits:
+        if s[i] != " ":
+            digits = digits[:10]
+            num += s[i]
+        i += 1
+        
+    try:
+        num = int(num)
+        num = 2**31 - 1 if num > 2**31 -1 else num
+        num = -2**31 if num < -2**31 else num
+    except:
+        return 0
+    print(num)
     return num
 
 convert_to_int(
-"")
-
-if "":
-    print('test')
+"4193 with words")
