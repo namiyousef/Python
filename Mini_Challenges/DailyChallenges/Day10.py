11066c3982adf6711e4eb79318baf0c3df00870e
Yousef <namiyousef@hotmail.com>
2020-10-26 10:27:45 +0000

    added problem 10

---
+++ Mini_Challenges/DailyChallenges/Day10.py
@@ -0,0 +1,25 @@
+"""
+Author: Yousef Nami
+Problem definition: https://leetcode.com/problems/palindrome-number/
+Date started: 26.10.2020
+Date complete: 
+Results: 
+
+"""
+
+
+def check_palindrome(x):
+    if x < 0:
+        return False
+    dig = []
+    while x != 0:
+        dig.append(x%10)
+        x = x//10
+    
+    for i in range(len(dig)//2):
+        print(dig[i],dig[len(dig) - i - 1])
+        if dig[i] != dig[len(dig) - i - 1]:
+            return False
+    return True
+    
+print(check_palindrome(1001))
