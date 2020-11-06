59d3b31cc13ce9c759e20ebabfd12ae31c84a645
Yousef <namiyousef@hotmail.com>
2020-11-06 07:23:02 +0100

    added comments

--- Mini_Challenges/DailyChallenges/Day13.py
+++ Mini_Challenges/DailyChallenges/Day13.py
@@ -4,17 +4,19 @@ Problem definition: https://leetcode.com/problems/trapping-rain-water/
 Date started: 06.11.2020
 Date complete: 
 Results: 
-    
+    - faster than 5.45%
+    - less memory than 87.19%
 """
 
 def find_water(height):
     i = 0
     water = 0
-    while i<len(height):
+    while i<len(height) - 1:
         if height[i]:
             j = i+1
-            while j != len(height) and height[j] < height[i] and height[i] <= max(height[i+1:]):
-                water += height[i] - height[j]
+            while j < len(height) - 1 and height[j] < height[i]:
+                #and height[i] <= max(height[i+1:])
+                water += min(height[i],max(height[j:])) - height[j]
                 j += 1
             i = j
         else:
@@ -22,9 +24,13 @@ def find_water(height):
     return water
 
 
-print('test',find_water(height = [0,1,0,2,1,0,1,3,2,1,2,1])) # 6
+print(find_water(height = [0,1,0,2,1,0,1,3,2,1,2,1])) # 6
 
 print(find_water(height = [4,2,0,3,2,5])) # 9
 
 
 print(find_water(height = [4,2,3])) # 1
+
+print(find_water(height = [0,1,0,2,1,0,1,3,2,1,2,1])) # 6
+
+print(find_water(height = [5,4,1,2])) # 1
